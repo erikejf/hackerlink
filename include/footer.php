2a14afe5ab2d9069cfb3a48a675cd7b5d663871a
Erik Finman <erikejf@botangle.com>
2014-10-18 14:07:22 -0700

    My Website!

---
+++ include/footer.php
@@ -0,0 +1,32 @@
+<footer class="text-center">
+        <div class="footer-above">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center Textstyle1">
+                    
+<p>Copyright 2014. All rights reserved.</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+   
+   
+
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+
+</body>
+
+</html>
